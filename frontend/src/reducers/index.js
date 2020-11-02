479eff632558996d4f4a8fc4a674889d8b4892b6
Daniel Wang <35549086+dnelw@users.noreply.github.com>
2020-11-01 21:51:21 -0500

    TODO-57 - Reset redux state after user logout (#41)
    Co-authored-by: Daniel Wang <dnelw@outlook.com>

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -5,11 +5,12 @@ import { acs } from "./acs";
 import { authPage } from "./auth-page";
 import { error } from "./error";
 import { success } from "./success";
-import { player } from "./team"
+import { player } from "./team";
 import { openCourtTimeline } from "./opencourt-timeline";
 import { createPost } from "./create-post";
+import { LOGOUT } from "../constants";
 
-export default combineReducers({
+const appReducer = combineReducers({
   auth,
   authPage,
   error,
@@ -20,3 +21,12 @@ export default combineReducers({
   createPost,
   form: formReducer,
 });
+
+export default (state, action) => {
+  switch (action.type) {
+    case LOGOUT:
+      return appReducer(undefined, action);
+    default:
+      return appReducer(state, action);
+  }
+};
